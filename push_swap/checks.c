f929306880da830f017cad80a25c633f5b1b7cfc
Yago Sanchez Guzmán <yagosg@macbook-pro-de-yago.home>
2023-10-05 14:17:14 +0200

    perf

--- push_swap/checks.c
+++ push_swap/checks.c
@@ -6,7 +6,7 @@
 /*   By: ysanchez <ysanchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 18:20:44 by ysanchez          #+#    #+#             */
-/*   Updated: 2023/10/02 16:51:27 by ysanchez         ###   ########.fr       */
+/*   Updated: 2023/10/05 14:11:45 by ysanchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ int	ft_checkvalid(char **argv)
 	int	i;
 
 	i = 1;
+	if (argv[i] && ft_strlen(argv[i]) == 0)
+		return (1);
 	while (argv[i])
 	{
 		if (ft_atoi(argv[i]) == 0 && ft_zerocmp(argv[i]) == 1)
